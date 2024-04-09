1d2c3bfbdd09664dae1b0c35c546cbaeb625b9a0
CandidoRafael <rafaeladelinoc@gmail.com>
2024-04-09 14:13:43 -0300

    try add routes on index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: 'Rota principal Express 2.0'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server está rodando porta 3000')
 })
