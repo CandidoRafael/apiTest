21f2c5460d21a2175425c4e68d44260d37844065
CandidoRafael <rafaeladelinoc@gmail.com>
2024-04-09 14:23:10 -0300

    removendo rota principal do index.js

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my API ')
-})
+
 
 app.listen(3000, () => {
     console.log('Server está rodando porta 3000')
